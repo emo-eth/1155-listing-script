8188d59a9262cf0e064cdedb24a3983d8d07404c
James Wenzel <james.wenzel@opensea.io>
2022-06-02 15:22:55 -0700

    one at a time

--- src/list.ts
+++ src/list.ts
@@ -4,8 +4,8 @@ import { config } from "dotenv";
 import { sleep } from "./util.js";
 
 config();
-config("secret.txt");
-config("env.txt");
+config(".env");
+// config(".secret");
 
 let providerUrl = process.env.ETH_RPC_URL;
 let apiKey = process.env.OPENSEA_API_KEY;
@@ -41,37 +41,36 @@ async function main(): Promise<any> {
   // expires in 1 month
   const expirationTime = listingTime + 60 * 60 * 24 * 30;
   let i = 0;
-  for (; i < numListings; i += 5) {
-    // listing starts in 10 seconds
-    if (i % 10 == 0) {
-      console.log(`listed ${i}`);
-    }
+  // for (; i < numListings; i += 5) {
+  //   // listing starts in 10 seconds
+  //   if (i % 10 == 0) {
+  //     console.log(`listed ${i}`);
+  //   }
 
-    const orderArgs = {
-      asset: { ...OpenSeaAsset, schemaName: "ERC1155" },
-      accountAddress,
-      startAmount: price,
-      listingTime,
-      expirationTime,
-    };
-    let promises = [];
-    for (let i = 0; i < 5; i++) {
-      promises.push(
-        seaport.createSellOrder(orderArgs).catch((err) => {
-          console.log("failed; decrementing counter");
-          i -= 1;
-        })
-      );
-      await sleep(111);
-    }
-    await Promise.all(promises);
-    // console.log(promises);
-  }
+  //   const orderArgs = {
+  //     asset: { ...OpenSeaAsset, schemaName: "ERC1155" },
+  //     accountAddress,
+  //     startAmount: price,
+  //     listingTime,
+  //     expirationTime,
+  //   };
+  //   let promises = [];
+  //   for (let i = 0; i < 5; i++) {
+  //     promises.push(
+  //       seaport.createSellOrder(orderArgs).catch((err) => {
+  //         console.log("failed; decrementing counter");
+  //         i -= 1;
+  //       })
+  //     );
+  //     await sleep(111);
+  //   }
+  //   await Promise.all(promises);
+  //   // console.log(promises);
+  // }
 
   while (i < numListings) {
     console.log(`listed ${i}`);
     await seaport.createSellOrder(orderArgs).then((err) => (i -= 1));
-    await sleep(101);
     i++;
   }
 }
